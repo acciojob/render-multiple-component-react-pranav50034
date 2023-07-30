5c4434037116153cd261a47bf13e8ff85403c996
Pranav Pokale <pranavpokale50034@gmail.com>
2023-07-30 12:19:29 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,24 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+     <div id="main">
+        <p data-ns-test="project-name">React Project</p>
+        <p data-ns-test="project-description">
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+           consequatur distinctio? Tempore sunt laudantium, optio cumque numquam
+           distinctio, doloribus quia dolore reprehenderit aliquam consequuntur?
+           Necessitatibus cumque nobis voluptates molestias, voluptas fugiat
+           quod ducimus reiciendis vero omnis a quos autem est consequuntur,
+           deserunt explicabo, ea aspernatur minima! Sapiente esse magnam iure
+           nesciunt, deserunt rem assumenda veniam ex alias architecto modi,
+           quis a fugiat ratione, tenetur incidunt repellat est dolore dolores?
+           Enim voluptate quis sint distinctio nisi delectus veritatis ratione
+           rerum illum odit! Excepturi ipsa velit explicabo ex tempora saepe
+           accusamus iure eum voluptatem iusto? Corrupti officia adipisci odit
+           modi excepturi eum!
+        </p>
+     </div>
+  );
 }
 
 
